d7457ac189239d27cb3f51485bbdbf5f21120444
Daniel Wiegand <daniel.a.wiegand@posteo.de>
2019-12-28 09:40:49 +0100

    Make the heatmap function

---
+++ climateR/R/heatmapEmissionBudgetLeft.R
@@ -0,0 +1,39 @@
+#' Heatmap: Budget left for all years
+#' @details Prepares the heatmap displaying the budget which is left for all countries and years
+#' @param data Data frame containing the calculated "just" emissions budget for each state. Needs to have columns "country" and "budget_left_perc"
+#' @param scales Values for color scale, with seven vakues
+#' @param from Start year
+#' @param to Final year
+#' @import dplyr, ggplot2
+#' @export
+
+
+heatmapEmissionBudgetLeft <- function(data, scales, from, to) {
+  data %>%
+    na.omit() %>%
+    ggplot() +
+    geom_tile_interactive(aes(x = country, y = year, fill = budget_left_perc, data_id = data_id,
+                              tooltip = paste0(country, " (", year, "): ", round(budget_left_perc, 1), "% of budget left")), color = "white") +
+    scale_fill_gradientn(colours = c("slateblue4", "red", "orange", "white", "lightgreen", "chartreuse3", "darkgreen"),
+                         values = scales,
+                         na.value = "grey", name = "% budget left") +
+    coord_flip() +
+    scale_y_continuous(breaks = seq(from, to, 1)) +
+    theme(
+      panel.background = element_rect(fill = "transparent"), # bg of the panel
+      panel.grid.major = element_blank(), # get rid of major grid
+      panel.grid.minor = element_blank(), # get rid of minor grid
+      panel.border = element_rect(colour = "transparent", fill = NA, size = 0),
+      plot.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+      # plot.title = element_text(size = 8),
+      # plot.subtitle = element_text(size = 6),
+      title = element_text(colour = "white"),
+      axis.text = element_text(colour = "white"),
+      axis.text.x = element_text(angle = 90),
+      legend.background = element_rect(fill = "transparent", colour = NA), # get rid of legend bg
+      legend.box.background = element_rect(fill = "transparent", colour = NA), # get rid of legend panel bg
+      legend.text = element_text(colour = "white"),
+      legend.key = element_blank() # Removes frame around boxes
+    ) +
+    labs(x = "", y = "Year", title = "Budget consumption", subtitle = "Percent of budget left per country and year")
+}
--- ui.R
+++ ui.R
@@ -16,6 +16,8 @@ library(ggiraph)
 library(leaflet)
 library(RColorBrewer)
 
+# UI ####
+
 ui <- fluidPage(theme = shinytheme("slate"),
                 
                 tags$head(
@@ -29,87 +31,104 @@ ui <- fluidPage(theme = shinytheme("slate"),
                   column(10,
                          
                          tabsetPanel(id = "tabset-panel",
-                           tabPanel("Status Quo",
-                                    
-                                    absolutePanel(draggable = T,
-                                                  
-                                                  wellPanel(
-                                                    
-                                                    sliderInput("base_year", "Select base year", min = 1950, max = 2017, value = 1992, step = 1),
-                                                    
-                                                    selectInput("selected_probability", "Select probability", choices = c("66%" = "66",
-                                                                                                                          "50%" = "50",
-                                                                                                                          "33%" = "33"),
-                                                                selected = "66%"),
-                                                    
-                                                    selectInput("selected_warming_degrees", "Select warming degrees",
-                                                                choices = c("1.5°C" = 1.5, "2°C" = 2),
-                                                                selected = "1.5°C"),
-                                                    
-                                                    selectInput("selected_calculation_approach", "Select a calculation approach",
-                                                                choices = c("Per capita approach" = "pca"),
-                                                                selected = "pca")
-                                                    
-                                                  ), style = "z-index: 10; opacity: 0.65;"
-                                                  
-                                    ),
-                                    
-                                    girafeOutput("barchart_continents", width = "90%"),
-                                    
-                                    absolutePanel(
-                                      actionButton("forwardPage2", "", icon = icon("chevron-right"), class = "scroll-button"),
-                                      top = "35%", right = "10%", fixed = T
-                                    )
-                                    
-                           ),
-                           
-                           tabPanel("Next page",
-                                    
-                                    girafeOutput("linechart_continents", width = "90%"),
-                                    
-                                    absolutePanel(
-                                      actionButton("forwardPage3", "", icon = icon("chevron-right"), class = "scroll-button"),
-                                      top = "35%", right = "10%", fixed = T
-                                    )
-                                    
-                                    ),
-                           
-                           tabPanel("Leaflet",
-                                    
-                                    leafletOutput("chloropleth_emissions_per_capita"),
-                                    
-                                    absolutePanel(
-                                      actionButton("forwardPage4", "", icon = icon("chevron-right"), class = "scroll-button"),
-                                      top = "35%", right = "10%", fixed = T
-                                    )
-                                    
-                           ),
-                           
-                           
-                           tabPanel("Budget left",
-                                    
-                                    girafeOutput("heatmap_budget_left_allyears"),
-                                    
-                                    absolutePanel(
-                                      actionButton("forwardPage5", "", icon = icon("chevron-right"), class = "scroll-button"),
-                                      top = "35%", right = "10%", fixed = T
-                                    )
-                         ),
-                         
-                         tabPanel("Leaders and laggards",
-                                  
-                                  girafeOutput("barchart_leaders_laggards"),
-                                  
-                                  absolutePanel(
-                                    actionButton("forwardPage6", "", icon = icon("chevron-right"), class = "scroll-button"),
-                                    top = "35%", right = "10%", fixed = T
-                                  )
+                                     
+                                     # Status quo ####
+                                     
+                                     tabPanel("Status Quo",
+                                              
+                                              absolutePanel(draggable = T,
+                                                            
+                                                            wellPanel("The world's greenhouse gas emissions rise steadily since the beginning of the industrial revolution. 
+                                                                       In the 20th century, this rise dramatically accelerated. 
+                                                                       The chart shows that emissions are distributed unevenly among regions and states."),
+                                                            style = "z-index: 10; opacity: 0.65;", top = "30%", left = "10%", fixed = T, width = "15%", align = "justify"
+                                                            
+                                              ),
+                                              
+                                              girafeOutput("barchart_continents", width = "90%"),
+                                              
+                                              absolutePanel(
+                                                actionButton("forwardPage2", "", icon = icon("chevron-right"), class = "scroll-button"),
+                                                top = "35%", right = "10%", fixed = T
+                                              )
+                                              
+                                     ),
+                                     
+                                     # Next page ####
+                                     
+                                     tabPanel("Next page",
+                                              
+                                              girafeOutput("linechart_continents", width = "90%"),
+                                              
+                                              absolutePanel(
+                                                actionButton("forwardPage3", "", icon = icon("chevron-right"), class = "scroll-button"),
+                                                top = "35%", right = "10%", fixed = T
+                                              )
+                                              
+                                     ),
+                                     
+                                     # Leaflet ####
+                                     
+                                     tabPanel("Leaflet",
+                                              
+                                              leafletOutput("chloropleth_emissions_per_capita"),
+                                              
+                                              absolutePanel(
+                                                actionButton("forwardPage4", "", icon = icon("chevron-right"), class = "scroll-button"),
+                                                top = "35%", right = "10%", fixed = T
+                                              )
+                                              
+                                     ),
+                                     
+                                     # Budget left ####
+                                     
+                                     tabPanel("Budget left",
+                                              
+                                              absolutePanel(draggable = T,
+                                                            
+                                                            wellPanel(
+                                                              
+                                                              sliderInput("base_year", "Select base year", min = 1950, max = 2017, value = 1992, step = 1),
+                                                              
+                                                              selectInput("selected_probability", "Select probability", choices = c("66%" = "66",
+                                                                                                                                    "50%" = "50",
+                                                                                                                                    "33%" = "33"),
+                                                                          selected = "66%"),
+                                                              
+                                                              selectInput("selected_warming_degrees", "Select warming degrees",
+                                                                          choices = c("1.27°C" = 1.27, "1.5°C" = 1.5, "1.75°C" = 1.75, "2°C" = 2),
+                                                                          selected = "1.5°C"),
+                                                              
+                                                              selectInput("selected_calculation_approach", "Select a calculation approach",
+                                                                          choices = c("Per capita approach" = "pca"),
+                                                                          selected = "pca")
+                                                              
+                                                            ), style = "z-index: 10; opacity: 0.65;", top = "44%", left = "3%", fixed = T
+                                                            
+                                              ),
+                                              
+                                              girafeOutput("heatmap_budget_left_allyears"),
+                                              
+                                              absolutePanel(
+                                                actionButton("forwardPage5", "", icon = icon("chevron-right"), class = "scroll-button"),
+                                                top = "35%", right = "10%", fixed = T
+                                              )
+                                     ),
+                                     
+                                     # Leaders and laggards ####
+                                     
+                                     tabPanel("Leaders and laggards",
+                                              
+                                              girafeOutput("barchart_leaders_laggards"),
+                                              
+                                              absolutePanel(
+                                                actionButton("forwardPage6", "", icon = icon("chevron-right"), class = "scroll-button"),
+                                                top = "35%", right = "10%", fixed = T
+                                              )
+                                     )
+                                     
                          )
-                         
-                         )
-                         # girafeOutput("bump_chart_emitters"),
-                         
-                         
+
                   )
                 )
 )
